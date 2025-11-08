e050b76b4851458e0825e49ebe97074d7fbf8087
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-08 20:23:54 +0530

    JSX with Styling CSS

--- myapp/src/core-concepts/03-jsx/JSXExample.jsx
+++ myapp/src/core-concepts/03-jsx/JSXExample.jsx
@@ -1,3 +1,9 @@
+import "../../App.css"
 export default function JSXExample() {
-    return <h1>This is JSX EXAMPLE</h1>;
+    //embedded expersion
+    const name = "karthik";
+    return <>
+    <h1 className="bg-color">This is JSX EXAMPLE {name} </h1>
+    <p style={{backgroundColor:'yellow', border:'1px solid black'}}>Hi im paragragh</p>
+    </>
 }
