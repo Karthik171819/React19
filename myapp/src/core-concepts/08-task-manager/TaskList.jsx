963e72cab3c4468d6a533db9cf0db8ece337518a
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-17 17:36:55 +0530

    Task Manager

--- myapp/src/core-concepts/08-task-manager/TaskList.jsx
+++ myapp/src/core-concepts/08-task-manager/TaskList.jsx
@@ -1,9 +1,23 @@
-export default function TaskList({ tasks }) {
+export default function TaskList({ tasks, toggleTask, deleteTask }) {
     return (
-        <ul>
+        <ul className="task-ul">
             {tasks.map((task, index) => (
-                <li key={index} className="task-li">
-                    {task.text}
+                <li
+                    onClick={() => toggleTask(task.id)}
+                    key={index}
+                    className={`task-li ${task.completed ? "completed" : ""}`}
+                >
+                    <span className="task-text">{task.text}</span>
+
+                    <span
+                        onClick={(e) => {
+                            e.stopPropagation();      // prevent toggle
+                            deleteTask(task.id);      // delete this task
+                        }}
+                        className="delete"
+                    >
+                        X
+                    </span>
                 </li>
             ))}
         </ul>
--- myapp/src/core-concepts/08-task-manager/TaskManager.jsx
+++ myapp/src/core-concepts/08-task-manager/TaskManager.jsx
@@ -10,11 +10,23 @@ export default function TaskManager() {
         const newTask = {id:Date.now(), text, completed: false};
         setTasks([...tasks, newTask])
     }
+
+    const toggleTask = (id) =>{
+        setTasks(tasks.map((task) => {
+         return task.id == id ? {...task, completed: !task.completed} : task
+        }))
+    }
+
+     const deleteTask = (id) => {
+        const filteredTasks = tasks.filter(task => task.id !== id);
+        setTasks(filteredTasks);
+    };
+
     return (
         <div>
             <h1>Task Manager</h1>
             <TaskForm addTask={addTask}/>
-            <TaskList tasks={tasks}/>
+            <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask}/>
             <p>Tasks: {tasks.length}</p>
         </div>
     );
