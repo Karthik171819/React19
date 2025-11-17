195bd1242764514abfb41fd247001f29d5115c42
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-17 13:28:08 +0530

    Task Form

--- myapp/src/core-concepts/08-task-manager/TaskForm.jsx
+++ myapp/src/core-concepts/08-task-manager/TaskForm.jsx
@@ -1,9 +1,11 @@
 export default function TaskForm() {
     return(
         <div>
-            <form>
+            <form action>
+                <div className="add-task-action">
                 <input type="text"  className="add-input" placeholder="Add a task"/>
                 <button type="submit" className="btn">ADD</button>
+                </div>
             </form>
         </div>
     )
