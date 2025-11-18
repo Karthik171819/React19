1cf8f411c26004a317d54a5cbe73d24a9e650b6e
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-18 14:17:57 +0530

    Hooks useState

--- myapp/src/hooks/01-usestate/useStateExample.jsx
+++ myapp/src/hooks/01-usestate/useStateExample.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
 export default function UseStateExample(params) {
+    const [state, setState] = useState(false);
+
+    function handlingClick() {
+        setState(!state); //toggle
+    }
     return <div>
-        <button>Show Text</button>
-        <p>This is a Text message it came by when you clicked button </p>
+        <button onClick={handlingClick}>Show Text</button>
+        {state && <p>This is a Text message it came by when you clicked button Its A useState </p>}
     </div>
 }
