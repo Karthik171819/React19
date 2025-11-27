b93eda7cc73fe74a08fbb2ea3f948dcaf740d54e
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-27 19:32:18 +0530

    Use Ref

--- myapp/src/hooks/07-useMemo/useMemo.jsx
+++ myapp/src/hooks/07-useMemo/useMemo.jsx
@@ -4,9 +4,14 @@ const items = ["Apple", "Bannana", "Grapes", "Mango"]
 
 export default function UseMemo(){
     const [search, setSearch] = useState("");
-    
+
+    const filteredItems = items.filter((item) =>  item.includes(search));
+
     return <div>
         <h1>UseMemo example</h1>
         <input onChange={(e) =>setSearch(e.target.value)}type="text" placeholder="search Fruits"/>
+        <ul>
+            {filteredItems.map(item => <li>{item}</li>)}
+        </ul>
     </div>
 }
