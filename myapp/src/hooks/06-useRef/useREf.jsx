9cb6ab2f9500562ce416b412519a055077429d62
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-27 18:51:14 +0530

    Use Ref

--- myapp/src/hooks/06-useRef/useREf.jsx
+++ myapp/src/hooks/06-useRef/useREf.jsx
@@ -1,5 +1,14 @@
+ import { useRef } from "react";
 export default function UseRef() {
+
+    const inputRef = useRef();
+
+    const handle = () => {
+        inputRef.current.focus();
+    }
     return<div>
         <h1> UseRef example</h1>
+        <input ref={inputRef}type="text" name="" placeholder='click button to get outline'/>
+        <button onClick={handle}>Focus</button>
     </div>
 }
