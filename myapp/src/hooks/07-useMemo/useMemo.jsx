d288136c7e0fbdbe6242dc31b245d43f7fcf54f8
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-28 12:29:06 +0530

    UseMemo

--- myapp/src/hooks/07-useMemo/useMemo.jsx
+++ myapp/src/hooks/07-useMemo/useMemo.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const items = ["Apple", "Bannana", "Grapes", "Mango"]
 
 export default function UseMemo(){
     const [search, setSearch] = useState("");
+    const [counter, setCounter] = useState(0);
 
-    const filteredItems = items.filter((item) =>  item.includes(search));
+
+    const filteredItems = useMemo(() => {
+        console.log("filtering");
+        return items.filter((item) =>  item.includes(search));
+    }, [search])
 
     return <div>
         <h1>UseMemo example</h1>
@@ -13,5 +18,8 @@ export default function UseMemo(){
         <ul>
             {filteredItems.map(item => <li>{item}</li>)}
         </ul>
+
+        <h3>Count:{counter}</h3>
+        <button onClick={() => setCounter(counter+1)}>Increase Count</button>
     </div>
 }
